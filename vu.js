546d050a625fb0a1c68e634f6e9ab30b3fe7e4ea
vucamle <vuptbd00080@fpt.edu.vn>
2022-11-13 23:29:13 +0700

    index.html

--- vu.js
+++ vu.js
@@ -3,36 +3,19 @@ let a = 'pham tuan vu';
 
 console.log(a.charAt(0).toUpperCase()+ a.slice(1,5) +a.charAt(5).toUpperCase()+ a.slice(6,10)+ a.charAt(10).toUpperCase()+ a.slice(11));
 // bt2
-let e = ' javascript is cool '
-let t = '';
-for(let b of e.trim()){
-    if(b == 'a'){
-        b = 4;
-    }else if( b == 'e'){
-        b = 3;
-    }else if(b == 'i'){
-        b = 1;
-    }else if(b == 'o'){
-        b = 0
-    }else if ( b == 's'){
-        b = 5;
-    }
-    t = t +b;
-}
-console.log(t)
-
+let z = 'javascript is cool'
+l = z.replace(/a/g,'4').replace(/e/g,'3').replace(/i/g,'1').replace(/o/g,'0').replace(/s/g,'5')
+console.log(l)
 // bt3
 
-        var r = new Number();
-        r = prompt("nhap")
-        var s = new Number();
-        s =     document.getElementById('put').value;
-        if(s.length >= 3 && s.endsWith('ing')){
-            s = s + 'ly';
-        }else if(s.length <3){
-          s = s;
-        }else{
-            s += "ing";
-        }  
-        alert('in ra' +s);
-document.write('in ra' +s);
+        
+
+let x = 'swim';
+if(x.length >= 3 && x.endsWith('ing')){
+    x = x + 'ly';
+}else if(x.length <3){
+  x = x;
+}else{
+    x += "ing";
+}  
+console.log(x)
